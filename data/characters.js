cbe4d8e54445c1702f6a97c276999caf97ccc4ab
Brogan Clements <brogancle@gmail.com>
2021-05-07 21:29:58 -0500

    Add function to add a win/loss combo to characters

--- data/characters.js
+++ data/characters.js
@@ -15,6 +15,10 @@ const characters = require("../config/mongoCollections").characters;
  */
 let charDB = null;
 
+async function checkDB() {
+	if (charDB === null) charDB = await characters();
+}
+
 function throwErr(func, reason) {
 	throw `characters.js (${func}): ${reason}`;
 }
@@ -31,7 +35,7 @@ async function initCharDB(charList) {
 	if (!charList || !Array.isArray(charList) || charList.length === 0) {
 		throwErr("initCharDB", "Must get a valid array");
 	}
-	if (charDB === null) charDB = await characters();
+	checkDB();
 
 	for (char of charList) {
 		if (Array.isArray(char)) {
@@ -44,9 +48,10 @@ async function initCharDB(charList) {
 
 /**
  * Adds a single character to the DB
- * Throws if the character could not be added
  * @param {string} displayName - to act as the displayed name of a character
  * @param {string} abrvName - string to act as the id of a character
+ * @returns the new character added to the db
+ * @throws when the character cannot be added
  */
 async function addChar(displayName, abrvName) {
 	if (
@@ -61,7 +66,7 @@ async function addChar(displayName, abrvName) {
 		throwErr("addChar", "Must be given valid abbreviated name");
 	}
 
-	if (charDB === null) charDB = await characters();
+	checkDB();
 
 	const newChar = {
 		displayName: displayName,
@@ -75,38 +80,88 @@ async function addChar(displayName, abrvName) {
 		throwErr("addChar", `Failed to add char ${displayName}`);
 	}
 
-	return 1;
+	return await getOneChar(abrvName);
 }
 
 /**
  * Gets one character using the display name or abbreviated name
- * Returns the char entry if found, otherwise null
  * @param {string} charName - character display name or abbreviated name
+ * @returns the char entry if found, otherwise null
  */
 async function getOneChar(charName) {
 	if (!charName || typeof charName !== "string" || !charName.trim()) {
 		throwErr("getOneChar", "Must be given valid character name");
 	}
 
-	if (charDB === null) charDB = await characters();
+	checkDB();
 
 	let charSearch = await charDB.findOne({
 		$or: [{ displayName: charName }, { abrvName: charName }],
 	});
 
-    return charSearch;
+	return charSearch;
 }
 
+/**
+ *
+ * @returns
+ */
 async function getAllChar() {
-	if (charDB === null) charDB = await characters();
+	checkDB();
 
-    let allChar = await charDB.find({}).toArray();
+	let allChar = await charDB.find({}).toArray();
 
-    return allChar;
+	return allChar;
+}
+
+/**
+ * Adds a win to the winner and a loss to the loser
+ * @param {string} winner displayName or abrvName of the winner
+ * @param {string} loser displayName or abrvName of the loser
+ * @returns A 2-array of the winner and loser's updated documents
+ * @throws either winner or loser is not found in the db or one could not be updated
+ */
+async function addWin(winner, loser) {
+	if (!winner || typeof winner !== "string" || !winner.trim()) {
+		throwErr("addWin", "Must be given valid winner character name");
+	}
+	if (!loser || typeof loser !== "string" || !loser.trim()) {
+		throwErr("addWin", "Must be given valid loser character name");
+	}
+	checkDB();
+
+	let win = await getOneChar(winner);
+	let loss = await getOneChar(loser);
+
+	if (!win) throwErr("addWin", "Winner could not be found in the db");
+	if (!loss) throwErr("addWin", "Loser could not be found in the db");
+
+	if (!win.wins[loss.displayName]) {
+		win.wins[loss.displayName] = 1;
+	} else {
+		win.wins[loss.displayName] += 1;
+	}
+
+	if (!loss.losses[win.displayName]) {
+		loss.losses[win.displayName] = 1;
+	} else {
+		loss.losses[win.displayName] += 1;
+	}
+
+	const updateWins = await charDB.updateOne({ _id: win._id }, { $set, win });
+	if (updateWins.matchedCount === 0)
+		throwErr("addWin", "Could not update wins");
+
+	const updateLoss = await charDB.updateOne({ _id: loss._id }, { $set, loss });
+	if (updateLoss.matchedCount === 0)
+		throwErr("addWin", "Could not update losses");
+
+	return [win, loss];
 }
 
 module.exports = {
 	addChar,
-    getOneChar,
-    getAllChar,
+	getOneChar,
+	getAllChar,
+	addWin,
 };
