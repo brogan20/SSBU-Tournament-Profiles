5b190ce203536fec0757318a9772acd35ccaec5c
klouie1092 <klouie@DESKTOP-F76Q6OI.localdomain>
2021-05-09 16:10:54 -0500

    Sorting can now be descending!

--- public/js/tablesorts.js
+++ public/js/tablesorts.js
@@ -1,8 +1,10 @@
 //Code taken from https://www.w3schools.com/howto/howto_js_sort_table.asp and modified for this use
 function sortTable(sortIndex) {
-  var table, rows, switching, i, x, y, shouldSwitch;
+  let table, rows, switching, i, x, y, shouldSwitch, dir, switchcount;
   table = document.getElementsByTagName("TABLE")[0];
   switching = true;
+  dir="asc";
+  switchcount = 0;
   while (switching) {
     switching = false;
     rows = table.rows;
@@ -11,14 +13,30 @@ function sortTable(sortIndex) {
       x = rows[i].getElementsByClassName(sortIndex)[0];
       y = rows[i + 1].getElementsByClassName(sortIndex)[0];
 
-      if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-        shouldSwitch = true;
-        break;
+      if(dir=="asc"){
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          shouldSwitch = true;
+          break;
+        }
+      } else{
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          shouldSwitch = true;
+          break;
+        }
       }
     }
     if (shouldSwitch) {
       rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
       switching = true;
+      console.log('hello')
+      switchcount++;
+    }
+    else {
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        console.log('hello2')
+        switching = true;
+      }
     }
   }
 }
