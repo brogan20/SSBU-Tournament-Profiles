8bb762ea3168a42c600fa7058460de0d43a7a6aa
klouie1092 <klouie@DESKTOP-F76Q6OI.localdomain>
2021-05-14 05:35:04 -0500

    changed some of tablesort to jquery

--- public/js/tablesearch.js
+++ public/js/tablesearch.js
@@ -3,6 +3,7 @@
         let hideCount = 0
         let showCount = 0
         $("tr").not(".header").each(function(){
+            //Uses the built in includes function to check if the result should be shown
             if($(this).children()[0].textContent.trim().toLowerCase().includes(searchTerm.toLowerCase())){
                 $(this).show()
                 showCount += 1
@@ -12,14 +13,18 @@
                 hideCount += 1
             }
         })
+        //Hides or shows relevant elements based on the results
         showCount == 0 ? $(".noResult").show() && $(".header").hide(): $(".noResult").hide();
         hideCount != 0? $(".clearResults").show() : $(".clearResults").hide();
     }
+
+    //Base case, makes a query based on the search term
     $(document).on("click",".search",function(event) {
         event.preventDefault()
         searchTable($("#search_term").val())
     })
     
+    //Clear results actually just queries for the empty string
     $(document).on("click",".clearResults",function(event){
         event.preventDefault()
         searchTable("")
--- public/js/tablesorts.js
+++ public/js/tablesorts.js
@@ -1,44 +1,46 @@
-//Code taken from https://www.w3schools.com/howto/howto_js_sort_table.asp and modified for this use
-function sortTable(sortIndex, table, isNumeric) {
-  let rows, switching, i, x, y, shouldSwitch, dir, switchcount;
-  switching = true;
-  dir="asc";
-  switchcount = 0;
-  while (switching) {
-    switching = false;
-    rows = table.rows;
-    for (i = 1; i < (rows.length - 1); i++) {
-      shouldSwitch = false;
-      x = rows[i].getElementsByClassName(sortIndex)[0];
-      y = rows[i + 1].getElementsByClassName(sortIndex)[0];
+(function ($){
+  //Code taken from https://www.w3schools.com/howto/howto_js_sort_table.asp and modified for this use
+  function sortTable(sortIndex, table, isNumeric) {
+    let rows, switching, i, x, y, shouldSwitch, dir, switchcount;
+    switching = true;
+    dir="asc";
+    switchcount = 0;
+    while (switching) {
+      switching = false;
+      rows = table.rows;
+      for (i = 1; i < (rows.length - 1); i++) {
+        shouldSwitch = false;
+        //Finds two consecutive rows to compare
+        x = rows[i].getElementsByClassName(sortIndex)[0];
+        y = rows[i + 1].getElementsByClassName(sortIndex)[0];
 
-      tempx = isNumeric? parseFloat(x.innerHTML.replace("%","")) : x.innerHTML.toLowerCase();
-      tempy = isNumeric? parseFloat(y.innerHTML.replace("%","")) : y.innerHTML.toLowerCase();
-      shouldSwitch = (tempx > tempy) ? (dir == "asc") : (dir == "desc");
-      if(shouldSwitch){
-        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-        switching = true;
-        switchcount++;
-        break;
+        //If the sort is numeric, uses the int/float value (without special characters)
+        //otherwise, just uses innerHTML to sort
+        tempx = isNumeric? parseFloat(x.innerHTML.replace("%","")) : x.innerHTML.toLowerCase();
+        tempy = isNumeric? parseFloat(y.innerHTML.replace("%","")) : y.innerHTML.toLowerCase();
+
+        //Checks if the two rows should switch
+        shouldSwitch = (tempx > tempy) ? (dir == "asc") : (dir == "desc");
+
+        //Makes the swap and continues
+        if(shouldSwitch){
+          rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+          switching = true;
+          switchcount++;
+          break;
+        }
       }
-    }
-    if(!shouldSwitch) {
+      //If no switches were made, checks if it needs to be descending and then sorts it descendingly
+      //otherwise, breaks the loop
       if (switchcount == 0 && dir == "asc") {
         dir = "desc";
         switching = true;
       }
     }
   }
-}
 
-document.onclick = function (e) {
-    e = e || window.event;
-    let element = e.target;
+  $(document).on("click",".sortButton, .sortButtonNumeric", function(){
+    sortTable($(this).html(), $(this).parents()[1], $(this).attr("class") == "sortButtonNumeric");
+  })
 
-    if(element.className == 'sortButton'){
-      sortTable(element.innerHTML, element.parentElement.parentElement, false);
-    }
-    if(element.className == 'sortButtonNumeric'){
-      sortTable(element.innerHTML, element.parentElement.parentElement, true);
-    }
-}
+})(jQuery);
