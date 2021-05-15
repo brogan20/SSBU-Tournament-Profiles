2660a996bdc3150281e804f4d8bc6d4b9509107a
Dylan Regan <4dylanregan@gmail.com>
2021-05-15 18:18:34 -0400

    front end signup

--- routes/login.js
+++ routes/login.js
@@ -2,20 +2,29 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const userData = require('../data/users.js');
-// const path = require('path');
 const xss = require('xss');
-
-//const saltRounds = 9;
 
 
 router.get('/', async (req, res) => {
     if (xss(req.session.user)) {
-        res.redirect("/")
+        res.redirect("/");
     } else {
         if (xss(req.session.error)) {
             res.status(401).render('others/login', {pageTitle: 'Login', error: xss(req.session.error)});
         } else {
             res.render('others/login', {pageTitle: 'Login', error: xss(req.session.error)});
+        }
+    }
+});
+
+router.get('/signup', async (req, res) => {
+    if (xss(req.session.user)) {
+        res.redirect("/");
+    } else {
+        if (xss(req.session.error)) {
+            res.status(401).render('others/signup', {pageTitle: 'Signup', error: xss(req.session.error)});
+        } else {
+            res.render('others/signup', {pageTitle: 'Signup', error: xss(req.session.error)});
         }
     }
 });
@@ -41,22 +50,6 @@ router.post('/', async (req, res) =>{
         res.redirect('/login');
     } 
 });
-    /*
-    try{
-        var user = await userData.getOneUser(username);
-    } catch(e){
-        res.status(401).render('others/login', {pageTitle: 'Login', loginFail: true})
-        return;
-    }
-    let match = bcrypt.compareSync(password, user.hashedPassword);
-    if(match){
-        req.session.user = user;
-        res.render('others/landing', {pageTitle: 'SSBU Tournament Profiles', landing: true});
-        return;
-    } else{
-        res.status(401).render('others/login', {pageTitle: 'Login', loginFail: true})
-    }
-}) */
 
 router.get('/logout', async(req, res) => {
     req.session.destroy();
